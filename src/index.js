198e2aab12df4fe61fbb7fe9e618439a0e68dbb2
github username <github email address>
2021-03-25 21:28:41 -0400

    Completed adding tasks.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+  const taskForm = document.querySelector("#create-task-form")
+  const taskList = document.querySelector("#tasks")
+
+  taskForm.addEventListener("submit", function(event) {
+    event.preventDefault()
+    // get the user input
+    const newTask = document.querySelector("#new-task-description").value
+    
+    // add it to the DOM
+    const taskItem = document.createElement("li")
+    taskItem.innerText = newTask
+
+    taskList.appendChild(taskItem)
+    taskForm.reset()
+  })
 });
